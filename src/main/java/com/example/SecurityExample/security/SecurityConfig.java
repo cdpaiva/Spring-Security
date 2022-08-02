1cae378681ff628521ac5b01293524d8803f4e49
cdpaiva <cdpaivaa@gmail.com>
2022-08-02 10:49:34 -0400

    Removes WebSecurityConfigurerAdapter

--- src/main/java/com/example/SecurityExample/security/SecurityConfig.java
+++ src/main/java/com/example/SecurityExample/security/SecurityConfig.java
@@ -4,45 +4,40 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.example.SecurityExample.filters.JwtRequestFilter;
 
-@SuppressWarnings("deprecation")
 @Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	
-	@Autowired
-	CustomUserDetailsService userDetailsService;
+public class SecurityConfig {
 	
 	@Autowired
 	JwtRequestFilter jwtRequestFilter;
-
-	@Override
-	public void configure(AuthenticationManagerBuilder authManager) throws Exception {
-		authManager.userDetailsService(userDetailsService);
-	}
 	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http
-			.csrf().disable()
-			.authorizeRequests()
-			.antMatchers("/user").hasRole("USER")
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		       
+        http
+        	.csrf().disable()
+        	.authorizeHttpRequests()
+        	.antMatchers("/user").hasRole("USER")
 			.antMatchers("/authenticate").permitAll()
 			.antMatchers("/").permitAll()
 			.anyRequest().authenticated()
-			.and().sessionManagement()
-			.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-		http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+			.and()
+			.sessionManagement()
+			.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and()
+			.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+        
+        return http.build();
+		
 	}
 	
 	@Bean
@@ -50,10 +45,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		return new BCryptPasswordEncoder();
 	}
 	
-	@Override
 	@Bean
-	public AuthenticationManager authenticationManagerBean() throws Exception {
-		return super.authenticationManagerBean();
+	public AuthenticationManager authenticationManager(
+			AuthenticationConfiguration authenticationConfiguration) throws Exception {
+	return authenticationConfiguration.getAuthenticationManager();
 	}
 
 }
